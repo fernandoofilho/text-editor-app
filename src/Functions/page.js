f1345f971cc8324a2cebe727ed1aa38b6fbe5623
Fernando Filho <faaf.eng19@uea.edu.br>
2024-03-27 00:40:22 -0400

    some fixes
    *  Icon bugs fixed
    * Functions now agregated to icons not in buttons
    * To be fixed: Function add page and delete page

--- src/Functions/page.js
+++ src/Functions/page.js
@@ -1,6 +1,6 @@
 
 var pageCount = 0;
-
+var pages = [];
 function saudacao(nome) {
     console.log("Olá, " + nome + "!");
     alert(nome);
@@ -18,50 +18,49 @@ function ajustarTamanho(elemento) {
 }
 
 
-function adicionarPrimeiraPag() {
+function adicionarPrimeiraPag(index) {
     var newPage = document.createElement('div');
     newPage.classList.add('paper-page');
     pageCount++;
-    newPage.id = 'page' + pageCount;
+    newIndex = index+1
+    newPage.id = 'page-' + newIndex;
+    pages.push(newPage.id);
+    console.log(pages);
     document.getElementById('pages').appendChild(newPage);
-
+    
     // add menu icons
-
     // icons area
     var iconArea = document.createElement('div');
     iconArea.classList.add('icon-area');
 
     newPage.appendChild(iconArea);
     // Adiciona os botões e ícones
-    // *********************************** //
-    var addPageButton = document.createElement('button');
-    addPageButton.classList.add('addPage-menu');
-    addPageButton.onclick = adicionarPrimeiraPag;
-    iconArea.appendChild(addPageButton);
 
+    // addPage *********************************** //
     var addPageIco = document.createElement('img');
     addPageIco.src = 'icons/adicionar-pagina.svg'; 
     addPageIco.classList.add('addPage-menu');
-    addPageButton.appendChild(addPageIco);
-
-    // ********************************** //
-    var expandPage = document.createElement('button');
-    expandPage.classList.add('expandPage-menu');
-    expandPage.onclick = expandView;
-    iconArea.appendChild(expandPage);
-
-    var expandPageIco = document.createElement('img');
-    expandPageIco.src = 'icons/expandir.svg';
-    expandPageIco.classList.add('expandPage-menu')
-
-    expandPage.onclick = function() {
-        expandView(this.parentNode.id, novoTamanhoDaDiv, novoTamanhoDasOutrasDivs);
+    
+    addPageIco.onclick = function() {
+        var pageIndex = Number(this.parentNode.id.replace("page-", "")) + 1;
+        console.log("Parent node: "+this.parentNode.id);
+        console.log(pageIndex);
+        adicionarPrimeiraPag(pageIndex);
     };
-    // *********************************** //
+
+    iconArea.appendChild(addPageIco);
+
+    // delete Page *********************************** //
     var deletePage = document.createElement('img');
     deletePage.src = 'icons/excluir.svg';
     deletePage.classList.add('deletePage-menu')
+    deletePage.id = "delete-"+newPage.id;
+    deletePage.onclick = function() {
+        console.log(this.parentNode.id);
 
+        var pageId = this.parentNode.id.replace('delete-', '');
+        removeDiv(pageId);
+    };
     iconArea.appendChild(deletePage);
 
 
@@ -71,17 +70,30 @@ function adicionarPrimeiraPag() {
 }
 function removeDiv(elementId) {
     var div = document.getElementById(elementId);
-    div.classList.add("remove");
-    setTimeout(function() {
-        div.remove();
-    }, 200);
+    if (div) {
+        div.classList.add("remove");
+        setTimeout(function() {
+            div.remove();
+        }, 200);
+    } else {
+        console.error('Elemento com ID ' + elementId + ' não encontrado.');
+    }
 }
-function expandView(elementId, newSize = 1280, otherSize= 748) {
+
+function expandView(elementId) {
+    var newSize = 1280;
+    var otherSize = 748;
+
     // Seleciona a div alvo
     var targetDiv = document.getElementById(elementId);
-    // Aumenta o tamanho da div alvo
-    targetDiv.style.width = newSize + 'px';
-    targetDiv.style.height = newSize + 'px';
+    // Verifica se o elemento foi encontrado antes de tentar acessar seu estilo
+    if (targetDiv) {
+        // Aumenta o tamanho da div alvo
+        targetDiv.style.width = newSize + 'px';
+    } else {
+        console.error('Elemento com ID ' + elementId + ' não encontrado.');
+        return; // Encerra a função se o elemento não for encontrado
+    }
 
     // Seleciona todas as divs com a mesma classe
     var allDivs = document.getElementsByClassName(targetDiv.className);
@@ -91,7 +103,19 @@ function expandView(elementId, newSize = 1280, otherSize= 748) {
         if (allDivs[i].id !== elementId) {
             // Diminui o tamanho das outras divs
             allDivs[i].style.width = otherSize + 'px';
-            allDivs[i].style.height = otherSize + 'px';
+        }
+    }
+}
+
+function reorderPages(index, operation) {
+    var offset = (operation === 'add') ? 1 : -1; 
+    for (var i = index + 1; i < pageCount; i++) {
+        var page = document.getElementById('page-' + i);
+        var pageDeleteButton = document.getElementById('delete-page-' + i);
+
+        if (page && pageDeleteButton) {
+            page.id = 'page-' + (i + offset);
+            pageDeleteButton.id = 'delete-page-' + (i + offset);
         }
     }
 }
